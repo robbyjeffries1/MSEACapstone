4e660bd55abb177b7120c8d5a1527adec68333f8
robbyjeffries1 <robby.jeffries@att.net>
2022-02-08 16:04:37 -0600

    Update EDA

--- Code/eda.py
+++ Code/eda.py
@@ -6,3 +6,23 @@ EDA
 Written by: Robby Jeffries
 Date written: 02-08-2022
 """
+
+# Import libraries
+import os
+import json
+
+# Set working directory
+os.chdir('/Users/robbyjeffries/MSEACapstone')
+print("Current working directory: {0}".format(os.getcwd()))
+
+
+# Opening JSON file
+with open('Data/AMAZON_FASHION_5.json', 'r') as f:
+  data = f.read()
+  
+# Parse file
+obj = json.loads(data)
+
+# Show values
+print("usd: " + str(obj['overall']))
+
